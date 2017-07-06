350493b7fd1beda285bc64d9bca9a16326546dcb
artak1990 <babayan-ararat@mail.ru>
2017-07-06 16:42:22 +0400

    auth -A

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <header>
+        <nav class="navbar dropdown-menu">
+            <ul class="nav-items">
+                <li class="nav-item my-logo"><a href="#"><img src="{{asset('img/logo.png')}}" alt="" class="my-logo"></a></li>
+                <li class="nav-item"><a href="#">Home</a></li>
+                <li class="nav-item dropdown">
+                    <a href="#">Product</a>
+                    <ul class="dropdown-content">
+                        <li class="dropdown2">
+                            <a href="#">Item 1</a>
+                            <ul class="dropdown-content2">
+                                <li><a href="#">Item 1</a></li>
+                                <li><a href="#">Item 2</a></li>
+                                <li><a href="#">Item 3</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown2">
+                            <a href="#">Item 2</a>
+                            <ul class="dropdown-content2">
+                                <li><a href="#">Item 1</a></li>
+                                <li><a href="#">Item 2</a></li>
+                                <li><a href="#">Item 3</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown2">
+                            <a href="#">Item 3</a>
+                            <ul class="dropdown-content2">
+                                <li><a href="#">Item 1</a></li>
+                                <li><a href="#">Item 2</a></li>
+                                <li><a href="#">Item 3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a href="#">About</a></li>
+                <li class="nav-item"><a href="#">Contact</a></li>
+                <li class="nav-item dropdown-right">
+                    <a href="#">Mycart</a>
+                    <ul class="dropdown-content-right">
+                        <li><a href="#">Item 1</a></li>
+                        <li>
+                            <a href="#">Dropdown</a>
+                        </li>
+                        <li>
+                            <a href="#">Dropdown</a>
+                        </li>
+                        <li><a href="#">Log out</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a href="#">Wish</a></li>
+                <li class="nav-item"><a href="#">Register/Login</a></li>
+            </ul>
+        </nav>
+    </header>
+</div>
